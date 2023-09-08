4bc336be0353861b0d4a2cf326cb505e03ea9411
haluk.dogan@dmo.gov.tr <haluk.dogan@dmo.gov.tr>
2023-09-08 14:32:05 +0300

    End of section 7

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -15,13 +15,19 @@ builder.Services.AddMassTransit(x =>
 {
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
 
-    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search",false));
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
 
-    x.UsingRabbitMq((context,cfg)=>
+    x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.ReceiveEndpoint("search-auction-created", e => 
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
         {
-            e.UseMessageRetry(r=>r.Interval(5,5));
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
 
             e.ConfigureConsumer<AuctionCreatedConsumer>(context);
         });
